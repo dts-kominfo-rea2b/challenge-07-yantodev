8915a0644fa6e1442120bb9162756003f7606125
yanto <ekocahyanto007@gmail.com>
2022-06-11 15:04:35 +0700

    challenge 7 promis

--- promise.js
+++ promise.js
@@ -1,7 +1,30 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+const getData = (data, findData) => {
+  return new Promise((resolve, reject) => {
+    if (findData == undefined) {
+      reject("Data is undefined")
+    } else {
+      let result = 0;
+      for (let i = 0; i < data.length; i++) {
+        if (findData == data[i]['hasil']) {
+          result++;
+        }
+      }
+      resolve(result);
+    }
+  })
+}
+
+const promiseOutput = async (findData) => {
+  const countTheaterIXX = await promiseTheaterIXX()
+    .then((hasilSetelahMenonton) => getData(hasilSetelahMenonton, findData))
+  const countTheaterVGC = await promiseTheaterVGC()
+    .then((hasilSetelahMenonton) => getData(hasilSetelahMenonton, findData))
+  return countTheaterIXX + countTheaterVGC
+}
 
 module.exports = {
   promiseOutput,
